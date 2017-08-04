478d41877bdb702e5dbad609be2b83d32e273a7c
Nathan R Smith <nrsmith012@gmail.com>
2017-08-04 14:09:06 -0700

    first

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ninjas/')
+def ninjas():
+    return render_template('ninjas.html')
+
+@app.route('/dojos/new/')
+def new_dojo():
+    return render_template('dojos.html')
+
+
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
+ 
